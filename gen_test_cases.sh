ad9c364f1a3f91a84699daa356abf7ec40491702
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2015-05-31 04:56:04 +0200

    Eliminate GNU extensions in test case generator

--- gen_test_cases.sh
+++ gen_test_cases.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
-maxbits=64
+maxbits=${1:-64}
+export POSIXLY_CORRECT=1 LC_ALL=C
+unset BC_ENV_ARGS BC_LINE_LENGTH
 {
 	echo 'scale= 0'
 	echo 'for (b= 0; b < '$maxbits'; ++b) {'
@@ -9,10 +11,19 @@ maxbits=64
 		"2 ^ b" \
 		"2 ^ b + 1"
 	do
-		echo '   print '"$x"', "\n"'
+		echo "   $x"
 	done
 	echo '}'
-} | bc | {
+} | bc | while read LINE
+do
+	s=`printf %s "$LINE" | tr -d '\\\'`
+	if test x"$s" != x"$LINE"
+	then
+		printf %s "$s"
+	else
+		printf '%s\n' "$LINE"
+	fi
+done | {
 	cat
 	for x in 12 17 99 2382837823782793 100000
 	do
