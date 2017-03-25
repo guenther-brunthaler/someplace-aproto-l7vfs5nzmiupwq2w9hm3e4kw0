73834bfae3500b9cf3ac70855d601a6aacaf1ee5
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2017-03-25 16:28:13 +0100

    Remove 0x prefixes from generated table
    Instead, convert all remaining value into hexadecimal and place a notice
    at the front of the table.

--- aproto_opcodes.sh
+++ aproto_opcodes.sh
@@ -13,12 +13,12 @@ exec awk -f - << EOF > "$o"
 	}
 	
 	function x(d) {
-		return sprintf("0x%02X", d)
+		return sprintf("%02X", d)
 	}
 	
 	function p7(pfx,    p) {
 		for (p= 1; p <= 64; p+= p) {
-			add_head(pfx " " p " octet" (p == 1 ? "" : "s"))
+			add_head(pfx " " x(p) " octet" (p == 1 ? "" : "s"))
 		}
 	}
 
@@ -30,21 +30,22 @@ exec awk -f - << EOF > "$o"
 			" (normally not required)")
 		s23= int((s= end - start) / 3)
 		for (s= s23 + s % 3; s--; ) {
-			add_head("data field payload with the" \
+			add_head("single-octet data field payload with" \
 				" implied value " x(start))
 		}
 		s23-= 7
 		for (s= 0; s < s23; ++s) {
 			add_head("data field payload follows, with length" \
-				" (<opcode> - 0x56 == " x(s) ") bytes")
+				" (<opcode> - 56 == " x(s) ") bytes")
 		}
-		p7("length-prefixed data field follows, prefix is")
+		p7("length-prefixed data field payload follows, prefix is")
 		for (s= 0; s < s23; ++s) {
-			add_head("tag increment with the implied" \
-				" value (<opcode> - 0xA8 == " x(s + 2) ")")
+			add_head("tag increment with implied" \
+				" value (<opcode> - A8 == " x(s + 2) ")")
 		}
-		p7("tag increment value follows as ")
+		p7("tag increment value follows as")
 		if (start != end) exit 1
+		print "Opcodes (all values are hexadecimal):"
 		for (start= 0; start < beyond; ++start) {
 			print x(start) " " r[start]
 		}
